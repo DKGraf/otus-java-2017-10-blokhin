41c5d1865b7c28613b661f5ba3468b693d067adc
Lorodis <dkgrafeve@gmail.com>
2017-10-22 08:19:02 +0700

    Some changes

--- OtusL02/src/main/java/ru/otus/l02/ObjectSizePrinter.java
+++ OtusL02/src/main/java/ru/otus/l02/ObjectSizePrinter.java
@@ -4,7 +4,7 @@ import com.carrotsearch.sizeof.RamUsageEstimator;
 
 /**Данный класс содержит метод, который выводит на экран
  * размер переданного ему объекта. Для этого используется
- * зависимоть на com.carrotsearch.java-sizeof
+ * зависимоть на артифакт java-sizeof от com.carrotsearch
  */
 
 public class ObjectSizePrinter {
