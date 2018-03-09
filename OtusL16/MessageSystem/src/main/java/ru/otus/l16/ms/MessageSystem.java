5f203fafb9a824a97a2726830adf3436e4e5b63c
DKGraf <semyon.blokhin@gmail.com>
2018-03-09 16:38:40 +0700

    This project now working. But only with one front and db.

--- OtusL16/MessageSystem/src/main/java/ru/otus/l16/ms/MessageSystem.java
+++ OtusL16/MessageSystem/src/main/java/ru/otus/l16/ms/MessageSystem.java
@@ -7,26 +7,70 @@ import java.io.IOException;
 
 public class MessageSystem {
     private static final int DEFAULT_STEP_TIME = 10;
-    private final MStoDBSocket msToDBSocket;
-    private final MStoFESocket msToFESocket;
+    private final MStoDBSocket msToDbSocket;
+    private final MStoFESocket msToFeSocket;
+
+    private static final String DB_START_COMMAND = "java -jar ./Database/target/Database-1.0-SNAPSHOT.jar";
+    private static final String FE_START_COMMAND = "java -jar ./Frontend/target/Frontend-1.0-SNAPSHOT.jar";
+    private static final int START_DELAY = 3000;
 
     public MessageSystem() {
-        msToDBSocket = new MStoDBSocket();
-        msToFESocket = new MStoFESocket();
+        msToDbSocket = new MStoDBSocket();
+        msToFeSocket = new MStoFESocket();
     }
 
     @SuppressWarnings("InfiniteLoopStatement")
     public void start() {
+        startDatabase();
+        startSocket();
+        startLoop();
+        startFrontend();
+    }
+
+    private void startDatabase() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(DB_START_COMMAND.split(" "));
+//            pb.inheritIO();
+            pb.start();
+            try {
+                Thread.sleep(START_DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startFrontend() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(FE_START_COMMAND.split(" "));
+//            pb.inheritIO();
+            pb.start();
+            try {
+                Thread.sleep(START_DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("All modules is started!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startSocket() {
         new Thread(
                 () -> {
                     try {
-                        msToFESocket.run();
+                        msToFeSocket.run();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
         ).start();
+    }
 
+    private void startLoop() {
         new Thread(
                 () -> {
                     while (true) {
@@ -34,7 +78,7 @@ public class MessageSystem {
                         String dbToMSMessage = Messages.getDbToMsMessage(0);
 
                         if (feToMSMessage != null) {
-                            msToDBSocket.getCacheStateJSON();
+                            msToDbSocket.getCacheStateJSON();
                         }
                         if (dbToMSMessage != null) {
                             Messages.addMsToFeMessage(0, dbToMSMessage);
